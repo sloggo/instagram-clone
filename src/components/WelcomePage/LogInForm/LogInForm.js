a54e6a7eac77a20a6b82f002c89406cb63129174
sloggo <70816908+sloggo@users.noreply.github.com>
2022-10-06 11:44:58 +0100

    Finish login form

--- src/components/WelcomePage/LogInForm/LogInForm.js
+++ src/components/WelcomePage/LogInForm/LogInForm.js
@@ -1,7 +1,14 @@
+import '../LogInForm/LogInForm.css'
+
 export default function LogInForm(){
     return(
         <form>
-            <input type='text'></input>
+            <div className='login-input-containers'>
+                <input className='login-input' placeholder='Phone number, username or email' type='text'></input>
+                <input className='login-input' placeholder='Password' type='text'></input>
+            </div>
+
+            <button className='login-submit' type='submit'>Log In</button>
         </form>
     )
 }
