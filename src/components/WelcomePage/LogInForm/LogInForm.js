e1887a87d91f32c51d8c8184d3be8cab6a521eac
sloggo <70816908+sloggo@users.noreply.github.com>
2022-10-06 20:47:17 +0100

    Add login with google and forgot password

--- src/components/WelcomePage/LogInForm/LogInForm.js
+++ src/components/WelcomePage/LogInForm/LogInForm.js
@@ -2,13 +2,25 @@ import '../LogInForm/LogInForm.css'
 
 export default function LogInForm(){
     return(
-        <form>
-            <div className='login-input-containers'>
-                <input className='login-input' placeholder='Phone number, username or email' type='text'></input>
-                <input className='login-input' placeholder='Password' type='text'></input>
+        <>
+            <form>
+                <div className='login-input-containers'>
+                    <input className='login-input' placeholder='Phone number, username or email' type='text'></input>
+                    <input className='login-input' placeholder='Password' type='text'></input>
+                </div>
+
+                <button className='login-submit' type='submit'>Log In</button>
+            </form>
+
+            <p className='login-or-container'><span className='login-or'>OR</span></p>
+
+            <div className='login-with-google-container'>
+                <img className='login-with-google-logo' src='./images/googlelogo.png'></img>
+                <p>Log In with Google</p>
             </div>
 
-            <button className='login-submit' type='submit'>Log In</button>
-        </form>
+            <p className='login-forgot'>Forgot Password?</p>
+        </>
+        
     )
 }
